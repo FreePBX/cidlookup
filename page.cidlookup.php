13849952604b0d5e634887427a927973cc16421f
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2011-12-17 21:02:34 +0000

    adds FREEPBX_IS_AUTH checking to most module files re #5478

--- page.cidlookup.php
+++ page.cidlookup.php
@@ -1,4 +1,5 @@
 <?php /* $Id */
+if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 //Copyright (C) 2006 WeBRainstorm S.r.l. (ask@webrainstorm.it)
 //
 //This program is free software; you can redistribute it and/or
