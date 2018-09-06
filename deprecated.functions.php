7f67612f5d3df33b4df17606a4ebc195e5be16d2
Andrew Nagy <andrew.nagy@the159.com>
2018-09-06 14:41:17 -0400

    [Module Tag script: cidlookup 15.0.3] FREEPBX-18139

--- deprecated.functions.php
+++ deprecated.functions.php
@@ -2,34 +2,34 @@
 
 function cidlookup_add($post){
     FreePBX::Modules()->deprecatedFunction();
-    return FreePBX::Calleridlookup()->add($post);
+    return FreePBX::Cidlookup()->add($post);
 }
 
 function cidlookup_edit($id, $post){
     FreePBX::Modules()->deprecatedFunction();
-    return FreePBX::Calleridlookup()->edit($id,$post);
+    return FreePBX::Cidlookup()->edit($id,$post);
 }
 
 
 function cidlookup_did_add($cidlookupid, $extension, $cidnum){
     FreePBX::Modules()->deprecatedFunction();
-    return FreePBX::Calleridlookup()->didAdd($cidlookupid, $extension, $cidnum);
+    return FreePBX::Cidlookup()->didAdd($cidlookupid, $extension, $cidnum);
 }
 
 function cidlookup_did_del($extension, $cidnum){
     FreePBX::Modules()->deprecatedFunction();
-    return FreePBX::Calleridlookup()->didDelete($extension, $cidnum);
+    return FreePBX::Cidlookup()->didDelete($extension, $cidnum);
 }
 
 
 function cidlookup_did_list($id = false){
     FreePBX::Modules()->deprecatedFunction();
-    return FreePBX::Calleridlookup()->didList($id);
+    return FreePBX::Cidlookup()->didList($id);
 }
 
 function cidlookup_del($id){
     FreePBX::Modules()->deprecatedFunction();
-    return FreePBX::Calleridlookup()->delete($id);
+    return FreePBX::Cidlookup()->delete($id);
 }
 
 function cidlookup_list($all = false){
